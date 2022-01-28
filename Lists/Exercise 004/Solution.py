a604d1a79c0b582a8945a25aaef2963e2c4f1208
Ian Chen <chenian317@gmail.com>
2022-01-28 12:42:06 -0300

    Exercise 004

---
+++ Lists/Exercise 004/Solution.py
@@ -0,0 +1,9 @@
+
+
+def find_largest(my_list):
+    return min(my_list)
+
+lst = [1, 2, 3, 4, 5, 6, 7, -9, 100, 34, 2354, 3456, 1, 345]
+
+print("The list is: ", lst)
+print("The largest element is ", find_largest(lst))
