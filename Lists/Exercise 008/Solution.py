eac0b4d755c61cb3fdabb9cda5f812366c833707
Ian Chen <chenian317@gmail.com>
2022-01-28 21:46:17 -0300

    Exercise 008

---
+++ Lists/Exercise 008/Solution.py
@@ -0,0 +1,8 @@
+
+my_list = ['Red', 'Green', 'Yellow', 'Blue', 'Red']
+
+print("The list is: {0}".format(my_list))
+if len(my_list) == 0:
+    print("The list is empty")
+else:
+    print("The list is not empty")
