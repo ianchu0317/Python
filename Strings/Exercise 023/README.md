70143f0fc3d677643d67645bd4ceb7940d94186a
Ian Chen <chenian317@gmail.com>
2022-02-02 16:52:19 -0300

    Exercise 023

---
+++ Strings/Exercise 023/README.md
@@ -0,0 +1,1 @@
+Write a Python program to remove a newline in Python.
---
+++ Strings/Exercise 023/Solution.py
@@ -0,0 +1,5 @@
+# Write a Python program to remove a newline in Python.
+
+
+if __name__ == '__main__':
+    print(" ".join("This is a string\nWith purpose of testing".splitlines()))
