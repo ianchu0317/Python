1c26b0ca9f421eb39e0246296e69331c22c33c72
Ian Chen <chenian317@gmail.com>
2022-02-03 11:40:59 -0300

    Exercise 002

---
+++ Sets/Exercise 002/Solution.py
@@ -0,0 +1,9 @@
+
+if __name__ == '__main__':
+    my_set = set(input("Input data for set, separated by space: ").split())
+    print("Entered set is {0}".format(my_set))
+
+    # Iterate through elements
+    print("\nElements are: ")
+    for element in my_set:
+        print(element)
