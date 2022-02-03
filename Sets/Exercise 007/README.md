4022a4e21e6055dd6e11546f738ab37893123eec
Ian Chen <chenian317@gmail.com>
2022-02-03 12:03:42 -0300

    Exercise 007

---
+++ Sets/Exercise 007/README.md
@@ -0,0 +1,1 @@
+Write a Python progbram to create a union of sets
---
+++ Sets/Exercise 007/Solution.py
@@ -0,0 +1,12 @@
+# Python program to create union of sets
+
+
+if __name__ == '__main__':
+    set1 = {1, 2, 3}
+    set2 = {2, 3, 4}
+    print("Set 1 is: ", set1)
+    print("Set 2 is: ", set2)
+
+    set3 = set1 | set2
+
+    print("Union of set1 and set2 is: ", set3)
