637715f996e80f2e42ff8a5006c013f7922e033c
Ian Chen <chenian317@gmail.com>
2022-02-03 11:50:48 -0300

    Exercise 004

---
+++ Sets/Exercise 004/README.md
@@ -0,0 +1,1 @@
+Write a Python program to remove item(s) from a given set
---
+++ Sets/Exercise 004/Solution.py
@@ -0,0 +1,8 @@
+
+
+
+set1 = {1, 2, 3, 4, 5}
+print("Set 1 is {0}".format(set1))
+element_to_remove = int(input("Input element to remove: "))
+set1.discard(element_to_remove)
+print("Set after removing {0} is {1}".format(element_to_remove, set1))
