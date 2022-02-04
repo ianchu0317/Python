d5ac8a246e1737f422caf966d195bfa0c91fdc38
Ian Chen <chenian317@gmail.com>
2022-02-04 11:11:59 -0300

    Exercise 002

---
+++ Tuples/Exercise 002/README.md
@@ -0,0 +1,1 @@
+Write a Python program to create a tuple with different data types
---
+++ Tuples/Exercise 002/Solution.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == '__main__':
+    a = ['a', 'b', 'c']
+    b = {"Green":1, "Red":2, "Blue":3}
+    c = {1, 2, 3}
+    d = (a, b, c)
+
+    print("List is ", a)
+    print("Dictionary is ", b)
+    print("Set is ", c)
+    print("Tuple is ", d)
