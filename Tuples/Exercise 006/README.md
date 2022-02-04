adb3ce3c87f5d0a74db08ca7ed5911a36007a529
Ian Chen <chenian317@gmail.com>
2022-02-04 14:49:49 -0300

    Exercise 006

---
+++ Tuples/Exercise 006/README.md
@@ -0,0 +1,1 @@
+Write a Python program to conver a tuple to a string
---
+++ Tuples/Exercise 006/Solution.py
@@ -0,0 +1,6 @@
+# Convert tuple to string
+
+if __name__ == '__main__':
+    t = ('a', 'b', 'c')
+    print("Tuple is ", t)
+    print("Tuple to string is ", "".join(list(t)))
