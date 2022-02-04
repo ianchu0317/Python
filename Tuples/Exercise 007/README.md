d0d4f414f61d4ca59eeab5d1bb7f4c607da2c279
Ian Chen <chenian317@gmail.com>
2022-02-04 14:53:16 -0300

    Exercise 007

---
+++ Tuples/Exercise 007/README.md
@@ -0,0 +1,1 @@
+Write a python program to get the 4th element and 4th element from last of a tuple.
---
+++ Tuples/Exercise 007/Solution.py
@@ -0,0 +1,10 @@
+# Get 4th and last 4th
+
+
+
+if __name__ == "__main__":
+    t = 1, 2, 3, 4, 5, 6, 7, 8, 9
+    print("Tuple is ", t)
+
+    print("4th element is ", t[3])
+    print("Last 4th element is ", t[-4])
