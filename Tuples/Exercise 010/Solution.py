ff19fb341bff87a8bed97036ecc56fad2ea47947
Ian Chen <chenian317@gmail.com>
2022-02-04 15:07:55 -0300

    Exercise 010

---
+++ Tuples/Exercise 010/Solution.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    t = 1, 2, 3, 4, 5, 6,
+
+    print("Tuple is ", t)
+    element = int(input("Input element to search : "))
+
+    print("Is {0} in tuple ? {1}".format(element, element in t))
